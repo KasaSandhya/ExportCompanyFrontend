e887c0d91f23c5579c031e5d1e5940b2c10eadff
padma <pkanakala@baylogictech.com>
2023-05-02 11:38:20 +0530

    added login functionality

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CommonService } from 'app/services/common.service';
+import {Router} from '@angular/router';
+import { NotificationsService } from 'app/services/notifications.service'; 
 
 @Component({
   selector: 'app-login',
@@ -9,17 +12,38 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
  
   loginForm: any = FormGroup;
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder, private commonService: CommonService,
+              private router: Router, private notification: NotificationsService) { 
 
   }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
+      userName: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
+
   login(){
-    console.log("in constructor", this.loginForm.value.email)
+    if(!this.loginForm.valid){
+       this.notification.showNotification("Please fill required elements", 'danger');
+       return false;
+    }
+    console.log("in constructor", this.loginForm.value.userName)
+    this.commonService.login(this.loginForm.value.userName, this.loginForm.value.password).subscribe((response) => {
+      console.log("response", response);
+      if(response !== null){
+        console.log("in if condition")
+        this.router.navigate(['/dashboard']);
+      }else{
+        this.notification.showNotification("Bad Credentials", 'danger');
+        this.router.navigate(['/signin']);
+      }
+    });
+    
+  }
+
+  logout() {
+    this.router.navigate(['/login']);
   }
 }
--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, catchError } from "rxjs/operators";
 import { commonHeaders } from "./common.headers";
+import {Router} from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ export class CommonService {
 
   baseUrl= "http://localhost:3000/"
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private router: Router) { }
 
   getOrdersInfo(id: any) {
         console.log("in the service method ", id)
@@ -31,4 +33,22 @@ export class CommonService {
       );
   }
 
+  login(username: any, password:any) {
+    console.log("in the service method ", username)
+      return this.http.get(this.baseUrl + `users/${username}/${password}`,{
+    headers: commonHeaders,
+  })
+  .pipe(
+    map((data) => {
+      console.log("data", data);
+      return data;
+    })
+  )
+  .pipe(
+    catchError((err) => {
+      console.log(err, "ERERER>>>>>>>>>>>>>>>>>");
+      return err;
+    })
+  );
+}
 }
